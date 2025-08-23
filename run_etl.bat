dd54b7f33cae801872482d539ded115b7d602259
vik-1971 <vkushanov@gmail.com>
2025-08-23 21:54:10 +0300

    Update run_etl.bat

--- run_etl.bat
+++ run_etl.bat
@@ -1,7 +1,19 @@
-```powershell
-docker run -it --rm `
-  -v "${PWD}:/work" `
-  --entrypoint="" `
-  apache/spark `
-  /opt/spark/bin/spark-submit `
-  /work/src/etl_pipeline.py
+@echo off
+echo.
+echo Запуск ETL-пайплайна на PySpark...
+echo ======================================
+echo.
+docker run -it --rm ^
+  -v "%cd%:/work" ^
+  --entrypoint="" ^
+  apache/spark ^
+  /opt/spark/bin/spark-submit ^
+  /work/src/etl_pipeline.py ^
+  /work/data/data.csv ^
+  /work/output
+
+echo.
+echo ETL завершён.
+echo Результаты в папке output.
+echo.
+pause
